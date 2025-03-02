5654e11fb76f9aa1f97db3f8e61b6e19e7ae897b
Adithya B Sankar <129651930+adisrm2023@users.noreply.github.com>
2025-03-03 02:22:53 +0530

    Create dash.tsx

---
+++ src/dash.tsx
@@ -0,0 +1,10 @@
+import { SettingsDialog } from "../../components/settings-dialog"
+
+export default function Page() {
+  return (
+    <div className="flex h-svh items-center justify-center">
+      <SettingsDialog />
+    </div>
+  )
+}
+
